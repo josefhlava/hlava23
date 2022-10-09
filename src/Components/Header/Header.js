78aa8d6ea2104b7d7344cc9ad852e72ddc2709a1
Josef Hlava <josee91@gmail.com>
2022-10-09 12:33:42 +0200

    created header and footer

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return (
+        <header>
+            <div id="logo">
+
+            </div>
+            <div id="menu">
+                <h1>Hello World</h1>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
